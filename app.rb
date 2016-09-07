e6f64b8c0473b0f41a1e3edf6d2243ad5b5ab063
Cooper Maa <coopermaa77@gmail.com>
2016-09-07 17:16:14 +0000

    Chaining Reloader, ShowExceptions and Lint middlewares

--- app.rb
+++ app.rb
@@ -2,5 +2,7 @@ require "rack"
 require "greeter"
 require "upcase"
 
-app = Upcase.new(Greeter.new)
+# Reloader has a 10 seconds cooldown by default
+# Change it to 0, so that it picks up the changes immediately
+app = Rack::Reloader.new(Rack::ShowExceptions.new(Rack::Lint.new(Upcase.new(Greeter.new))), 0)
 Rack::Server.start app: app, Port: 8080
